b975bb4b7cfe67badef3a5141634a0309312aff2
Amandine Drebes <amandine@energie-ip.com>
2019-08-02 12:28:34 +0200

    Prepare PTM group command

--- internal/core/switch.go
+++ internal/core/switch.go
@@ -9,11 +9,14 @@ import (
 )
 
 type SwitchCmd struct {
-	Group     int  `json:"group"`
-	Leds      *int `json:"leds,omitempty"`
-	Slats     *int `json:"slats,omitempty"`
-	Blinds    *int `json:"blinds,omitempty"`
-	TempShift *int `json:"heat,omitempty"` //temperature shift in 1/10°C
+	Group     int   `json:"group"`
+	Leds      *int  `json:"leds,omitempty"`
+	Slats     *int  `json:"slats,omitempty"`
+	Blinds    *int  `json:"blinds,omitempty"`
+	TempShift *int  `json:"heat,omitempty"`     //temperature shift in 1/10°C
+	Action    *bool `json:"action,omitempty"`   //true/false   (press/release)
+	ButtonA   *bool `json:"button_A,omitempty"` //true/false  (0/1)
+	ButtonB   *bool `json:"button_B,omitempty"` //true/false  (0/1)
 }
 
 func (s *Service) onSwitchCmd(client network.Client, msg network.Message) {
