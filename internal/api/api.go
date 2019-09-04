47b22f96dfbe0b300f1178ca8f6124d2f5f4d0d6
Amandine Drebes <amandine@energie-ip.com>
2019-09-04 17:51:20 +0200

    Update default path

--- internal/api/api.go
+++ internal/api/api.go
@@ -164,7 +164,7 @@ func (api *API) getV1HVACConsumptions(w http.ResponseWriter, req *http.Request) 
 
 func (api *API) swagger() {
 	router := mux.NewRouter()
-	sh := http.StripPrefix("/swaggerui/", http.FileServer(http.Dir("/media/userdata/www/swaggerui/")))
+	sh := http.StripPrefix("/swaggerui/", http.FileServer(http.Dir("/data/www/swaggerui/")))
 	router.PathPrefix("/swaggerui/").Handler(sh)
 
 	// API v1.0
